4bb5df7824bb3df8f2c2b342a7b63980187db3b4
Jacek <jacek@sysop.cat>
2019-07-09 10:13:27 +0200

    preparing autostart services in container `

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,20 @@
 FROM debian:latest
 COPY . /app
 ADD crontab /etc/cron.d/cron-scan
-RUN chmod 0644 /etc/cron.d/cron-scan && touch /var/log/cron.log
+#VOLUME /usr/share/nginx/html
+EXPOSE 80
+RUN chmod 0644 /etc/cron.d/cron-scan && touch /var/log/cron.log ; chmod 0644 /app/start.sh 
 RUN apt-get update && apt-get install -y \
   wget \
   curl \
   gnuplot \
   bash    \
   cron  \
+  procps \
   nginx  && rm -rf /var/lib/apt/lists/* 
 
+
 WORKDIR /usr/share/nginx/html
-CMD cd /usr/share/nginx/html &&  nginx -g 'daemon off;' 	
+#CMD cd /usr/share/nginx/html &&  nginx -g 'daemon off;'
+#CMD ["cron","-f"]
+CMD /app/start.sh
---
+++ start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+nginx -g 'daemon off;' &
+status=$?
+if [ $status -ne 0  ]; then
+      echo "Failed to start my_first_process: $status"
+        exit $status
+fi
+
+
+/bin/bash /app/script.sh &
+status=$?
+if [ $status -ne 0  ]; then
+      echo "Failed to start my_second_process: $status"
+        exit $status
+fi
+
+
+
+while sleep 60; do
+      ps aux |grep nginx |grep -q -v grep
+        PROCESS_1_STATUS=$?
+          ps aux |grep script |grep -q -v grep
+            PROCESS_2_STATUS=$?
+              # If the greps above find anything, they exit with 0 status
+                # If they are not both 0, then something is wrong
+                  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0  ]; then
+                          echo "One of the processes has already exited."
+                              exit 1
+                                fi
+                            done
